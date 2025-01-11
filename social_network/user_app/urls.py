9b685a5d81b59d4d0be5c9e7ef4cc2b7246d8542
Dima <you@example.com>
2025-01-11 16:51:34 +0100

    Create logout

--- social_network/user_app/urls.py
+++ social_network/user_app/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('registration/', render_registration),
     path('login/', render_login, name="login"),
-    path('welcome/', render_welcome, name='welcome')
+    path('welcome/', render_welcome, name='welcome'),
+    path('logout_user/', logout_user, name = 'logout')
 ]
--- social_network/user_app/views.py
+++ social_network/user_app/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def render_registration(request):
+    # Змінна, що відпвідає за текст помилки. Ця змінна передається через context та відображається у шаблоні
     error = ""
+    # Якщо відправляється запит з типом POST (якщо відправляється форма)
     if request.method == "POST":
+        # Отримуємо дані з форми та зберігаємо у змінні
         username= request.POST.get("username")
         password= request.POST.get("password")
         confirm_password= request.POST.get("confirm_password")
+        # Якщо користувач двічі ввів однаковий пароль
         if password == confirm_password:
+            # Відлювлюємо помилку у разі, якщо користувач вже існує
             try: 
+                # Створення користувача у БД
                 User.objects.create_user(username=username, password=password)
+                # Перенапрвлення на сторінку логіну
                 return redirect("login")
             except IntegrityError:
                 error = "This user already exists"
@@ -26,13 +33,26 @@ def render_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        # Функція authenticate повертає об'єкт користувача, якщо юзер з такими логіном та паролем існує. Інкаше повртає None
         user = authenticate(request, username= username, password = password)
+        # Якщо користувач існує (користувачч ввів усі дані правильно у формі)
         if user != None:
+            # Авторизуємо користувача
             login(request, user)
+            # Перенаправляємо користувача на "сторінку-привітання"
             return redirect('welcome')
         else:
             error = "Username or password is not correct"
     return render(request, "user_app/login.html", context = {"error": error})
 
 def render_welcome(request):
-    return render(request,'user_app/welcome.html')
+    # Перевіяємо, чи авторизувався користувач
+    if request.user.is_authenticated:
+        return render(request,'user_app/welcome.html')
+    else:
+        return redirect("login")
+    
+def logout_user(request):
+    # Вихід із акаунту
+    logout(request)
+    return redirect('login')
